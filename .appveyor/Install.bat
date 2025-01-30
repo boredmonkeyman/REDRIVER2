08a9a4a5b14d2408219dc2b5f272da81ab0a893d
boredmonkeyman <164119563+boredmonkeyman@users.noreply.github.com>
2025-01-30 05:30:05 +0000

    Update Install.bat

--- .appveyor/Install.bat
+++ .appveyor/Install.bat
@@ -1,13 +1,37 @@
-@echo off
+$windows_premake_url = 'https://github.com/premake/premake-core/releases/download/v5.0.0-beta1/premake-5.0.0-beta1-windows.zip'
+$windows_jpeg_url = 'http://www.ijg.org/files/jpegsr9d.zip'
+$windows_openal_url = 'https://openal-soft.org/openal-binaries/openal-soft-1.21.1-bin.zip'
+$windows_sdl2_url = 'https://www.libsdl.org/release/SDL2-devel-2.0.20-VC.zip'
 
-appveyor DownloadFile %windows_premake_url% -FileName premake5.zip
-7z x premake5.zip -o%project_folder%
+$project_folder = '.\\src_rebuild'
+$dependency_folder = $project_folder + '\\dependencies'
 
-appveyor DownloadFile %windows_jpeg_url% -FileName JPEG.zip
-7z x JPEG.zip -o%dependency_folder%
+# Download required dependencies
+Invoke-WebRequest -Uri $windows_premake_url -OutFile PREMAKE.zip
+Expand-Archive PREMAKE.zip -DestinationPath $project_folder
 
-appveyor DownloadFile %windows_openal_url% -FileName OPENAL.zip
-7z x OPENAL.zip -o%dependency_folder%
+Invoke-WebRequest -Uri $windows_sdl2_url -OutFile SDL2.zip
+Expand-Archive SDL2.zip -DestinationPath $dependency_folder
 
-appveyor DownloadFile %windows_sdl2_url% -FileName SDL2.zip
-7z x SDL2.zip -o%dependency_folder%
+Invoke-WebRequest -Uri $windows_openal_url -OutFile OPENAL.zip
+Expand-Archive OPENAL.zip -DestinationPath $dependency_folder
+
+Invoke-WebRequest -Uri $windows_jpeg_url -OutFile JPEG.zip
+Expand-Archive JPEG.zip -DestinationPath $dependency_folder
+
+# Generate project files
+$windows_jpeg_dir = '.\\dependencies\\jpeg-9d'
+$windows_openal_dir = '.\\dependencies\\openal-soft-1.21.1-bin'
+$windows_sdl2_dir = '.\\dependencies\\SDL2-2.0.20'
+
+$env:SDL2_DIR = $windows_sdl2_dir
+$env:OPENAL_DIR = $windows_openal_dir
+$env:JPEG_DIR = $windows_jpeg_dir
+
+Set-Location -Path $project_folder
+Rename-Item -Path ($windows_jpeg_dir + '\\jconfig.vc') -NewName "jconfig.h"
+
+& .\\premake5 vs2019
+
+# Open solution
+& .\\project_vs2019_windows\\REDRIVER2.sln
